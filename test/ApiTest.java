27a21c4d0780ca5c0e1ce10258d479f97f8759bc
kkallu@ttu.ee <Jevgeni1416751>
2017-09-25 12:55:16 +0300

    initial commitQQ

---
+++ test/ApiTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+
+public class ApiTest {
+    @Test
+    public void gotWeatherInfoFromAPI() {
+
+
+    }
+
+    @Test
+    public void correctAmountOfWeatherForecasts() {
+
+    }
+
+    @Test
+    public void correctTemperatureAmplitude() {
+
+    }
+
+    @Test
+    public void correctDailyLowestTemp() {
+
+    }
+
+    @Test
+    public void correctDailyHighestTemp() {
+
+    }
+
+    @Test
+    public void correctCoordinates() {
+
+    }
+
+}
